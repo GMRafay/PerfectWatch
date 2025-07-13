7b8ea58f50d9ed43f706ca223bd40724fd6853eb
GMRafay <arafay0110@gmail.com>
2025-07-12 20:31:41 -0600

    added functions to popup component

--- perfect-watch/src/components/Popup.jsx
+++ perfect-watch/src/components/Popup.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
+import {
+  fetchMovieDetails,
+  fetchMovieRecommendations,
+  fetchSimilarMovies,
+} from "../services/tmdb";
+
 export function Popup({ movie_id }) {
+  const [movieDetails, setMovieDetails] = useState([]);
+  const [similarMovies, setSimilarMovies] = useState([]);
+  const [recommendations, setRecommendations] = useState();
+  useEffect(() => {
+    async function getMovieData() {
+      try {
+        const details = await fetchMovieDetails(movie_id);
+        const similar = await fetchSimilarMovies(movie_id);
+        const recommend = await fetchMovieRecommendations(movie_id);
+        if (details) {
+          setMovieDetails(details);
+        }
+        if (similar) {
+          setSimilarMovies(similar);
+        }
+        if (recommend) {
+          setRecommendations(recommend);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getMovieData();
+  }, [movie_id]);
+
   return (
-    <div className="bg-white border-white rounded-xl z-3 fixed top-50 sm:w-10 sm:h-10 md:w-100 md:h-100 overscroll-auto  md:overscroll-contain">
-      <p>This is the current selected movie id : {movie_id}</p>
+    <div className="bg-white border-white rounded-xl z-3 fixed top-25 bottom-100 sm:w-10 sm:h-10 md:w-200 md:h-180 overscroll-auto  md:overscroll-contain">
+      <div className="flex flex-col">
+        <img src=""></img>
+      </div>
     </div>
   );
 }
