b912f5456d43abd984460b3572ae2573aeded05e
GMRafay <arafay0110@gmail.com>
2025-07-17 18:14:59 -0600

    added a transition effect when switching from movies to picks

--- perfect-watch/src/App.jsx
+++ perfect-watch/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import MovieContent from "./components/MovieContent";
 import TvShowsContent from "./components/TvShowsContent";
 import Picks from "./components/Picks";
+import { motion, AnimatePresence } from "motion/react";
 
 function App() {
   const [content, setContent] = useState("movies");
@@ -11,11 +12,31 @@ function App() {
   return (
     <div className="bg-linear-to-b from-white-400 to-indigo-400 ">
       <Header setContent={setContent} />
-      {content == "movies" && (
-        <MovieContent setPicks={setPicks} picks={picks} />
-      )}
-      {content == "shows" && <TvShowsContent />}
-      {content == "picks" && <Picks picks={picks} />}
+      <AnimatePresence mode="wait">
+        {content === "movies" && (
+          <motion.div
+            key="movies"
+            initial={{ opacity: 0, transform: "translateX(-50px)" }}
+            animate={{ opacity: 1, transform: "translateX(0px)" }}
+            exit={{ opacity: 0, transform: "translateX(50px)" }}
+            transition={{ duration: 0.3 }}
+          >
+            <MovieContent setPicks={setPicks} picks={picks} />
+          </motion.div>
+        )}
+
+        {content === "picks" && (
+          <motion.div
+            key="picks"
+            initial={{ opacity: 0, transform: "translateX(50px)" }}
+            animate={{ opacity: 1, transform: "translateX(0px)" }}
+            exit={{ opacity: 0, transform: "translateX(-50px)" }}
+            transition={{ duration: 0.3 }}
+          >
+            <Picks picks={picks} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
