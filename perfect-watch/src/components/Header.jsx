8323b1c2d694a24dde61c9e31fd748ee78527a2d
GMRafay <arafay0110@gmail.com>
2025-07-23 16:43:34 -0600

    changed the styling to the header

--- perfect-watch/src/components/Header.jsx
+++ perfect-watch/src/components/Header.jsx
@@ -4,7 +4,10 @@ export default function Header({ setContent, content }) {
   return (
     <div className="w-screen overflow-x-hidden">
       <div className="flex flex-col w-full items-center">
-        <header className="m-10 text-center">What2Watch BRUHHH</header>
+        <h1 className="text-5xl m-5 font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-purple-600">
+          Perfect<span className="animate-pulse">Watch</span>
+        </h1>
+
         <Navbar setContent={setContent} content={content} />
       </div>
     </div>
